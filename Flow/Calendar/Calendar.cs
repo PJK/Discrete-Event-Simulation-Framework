1ab11e3537605a0ac89a09b629443102ce922880
PJK <me@pavelkalvoda.com>
2011-01-27 18:12:20 +0100

    calendar

---
+++ Flow/Calendar/Calendar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DESF.Flow.Calendar
+{
+    /// <summary>
+    /// The heart of simulation proccess. Registers and invokes
+    /// terms, triggers "global" events and manages queues.
+    /// </summary>
+    class Calendar : Event.IEventEmmiter
+    {
+
+        protected Event.EventEmmitingProvider _provider = new Event.EventEmmitingProvider();
+
+        public void AttachHandler(Event.IEventHandler handler)
+        {
+            _provider.AttachHandler(handler);
+        }
+
+        public void DetachHandler(Event.IEventHandler handler)
+        {
+            _provider.DetachHandler(handler);
+        }
+
+        public void AddTerm(Term term)
+        {
+        }
+
+    }
+}
---
+++ Flow/Calendar/Term.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DESF.Flow.Calendar
+{
+    public class Term
+    {
+        protected uint _time;
+        protected string _state;
+        protected HashSet<string> _data;
+        public uint Time
+        {
+            get
+            {
+                return _time;
+            }
+            protected set
+            {
+                throw new AccessViolationException("Term is locked for modifications!");
+            }
+        }
+
+        public string State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                throw new AccessViolationException("Term is locked for modifications!");
+            }
+        }
+
+        public HashSet<string> Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                throw new AccessViolationException("Term is locked for modifications!");
+            }
+        }
+
+        public Term(uint time, string state, HashSet<string> data)
+        {
+            _time = time;
+            _state = state;
+            _data = data;
+        }
+    }
+}
