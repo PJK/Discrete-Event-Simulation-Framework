b24ed3e8b00c74aaa689e4148f467a2de0a07446
PJK <me@pavelkalvoda.com>
2011-01-27 22:08:01 +0100

    calendar events

--- Flow/Calendar/Calendar.cs
+++ Flow/Calendar/Calendar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 
@@ -12,7 +13,7 @@ namespace DESF.Flow.Calendar
     class Calendar : Event.IEventEmmiter
     {
 
-        protected Term[] _terms;
+        protected List<Term> _terms = new List<Term>();
         protected uint _time = 0;
 
         protected Event.EventEmmitingProvider _provider = new Event.EventEmmitingProvider();
@@ -39,7 +40,7 @@ namespace DESF.Flow.Calendar
             }
             else
             {
-                _terms[_terms.Length + 1] = term;
+                _terms.Add(term);
             }
         }
 
@@ -47,6 +48,33 @@ namespace DESF.Flow.Calendar
         {
             _time = term.Time;
             term.Owner.Notify(new Event.SEvent(term.State, term.Data), this);
+        }
+
+        public void StartSimulation()
+        {
+            _provider.FireEvent(new Event.SEvent("SimulationStarted", null), this);
+        }
+
+        public void StopSimulation()
+        {
+            _provider.FireEvent(new Event.SEvent("SimulationStopped", null), this);
+        }
+
+        public bool HasNextTerm()
+        {
+            return _terms.Count > 0;
+        }
+
+        public void Procceed()
+        {
+            _terms.Sort();
+            uint otime = _terms[0].Time;
+            while (_terms[0].Time == otime)
+            {
+                Term tcache = _terms[0];
+                _terms.RemoveAt(0);
+                InvokeTerm(tcache);
+            }
         }
     }
 }
