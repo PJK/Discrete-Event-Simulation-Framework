b23a031a81657c2e2d099598eba821b54c0aa1be
PJK <me@pavelkalvoda.com>
2011-01-30 22:36:53 +0100

    added some code

---
+++ Flow/Event/ReflectiveEventDispatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DESF.Flow.Event
+{
+    /// <summary>
+    /// Provides reflection-based dispaching to discrete
+    /// handling methods
+    /// </summary>
+    public static class ReflectiveEventDispatcher
+    {
+        /// <summary>
+        /// Atempts to forward the passed event to appropriate
+        /// handler inside current class
+        /// </summary>
+        /// <param name="ev">The event</param>
+        /// <param name="source">The emmiting object</param>
+        /// <param name="handler">Object to handler the event</param>
+        public static void Notify(this IEventHandler handler, SEvent ev, IEventEmmiter source)
+        {
+            if (handler.GetType().GetMethod("Handle" + ev.Name) != null)
+            {
+                // C# WTF?
+                object[] prms = new object[] {ev, source};
+                handler.GetType().GetMethod("Handle" + ev.Name).Invoke(handler, prms);
+            }
+            else
+            {
+                throw new System.NotImplementedException("This object doesn't have appropriate handler methods for event " + ev.Name);
+            }
+        }
+    }
+}
---
+++ Tools/ObjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace DESF.Tools
+{
+    public class ObjectRepository
+    {
+        protected Hashtable _data = new Hashtable();
+
+        public void Set(string key, object item)
+        {
+            _data[key] = item;
+        }
+
+        public T Get<T>(string key)
+        {
+            if (_data[key] is T)
+            {
+                return (T)_data[key];
+            }
+            else
+            {
+                throw new ArgumentException("Requested item has different type that the referenced one!");
+            }
+        }
+
+    }
+}
